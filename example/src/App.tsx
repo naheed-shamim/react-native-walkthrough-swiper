85623a037d9ce5f4e8b757aa0a07d4f5d5b1b2de
Naheed Shamim <n.shamim-e@arada.com>
2023-08-30 16:05:13 +0400

    App.tsx changes

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,31 @@
-import * as React from 'react';
+// import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-walkthrough-swiper';
+// import { StyleSheet, View, Text } from 'react-native';
+// import { multiply } from 'react-native-walkthrough-swiper';
 
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
+// export default function App() {
+//   const [result, setResult] = React.useState<number | undefined>();
 
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
+//   React.useEffect(() => {
+//     multiply(3, 7).then(setResult);
+//   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
+//   return (
+//     <View style={styles.container}>
+//       <Text>Result: {result}</Text>
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   box: {
+//     width: 60,
+//     height: 60,
+//     marginVertical: 20,
+//   },
+// });
