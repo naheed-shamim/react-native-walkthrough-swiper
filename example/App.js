91ef3b58c6aefb63bb2d3644eec3b3cc69f44b2a
Naheed Shamim <>
2023-10-26 15:20:27 +0400

    updated main App.js

--- example/App.js
+++ example/App.js
@@ -6,60 +6,68 @@
  */
 
 import React from 'react';
-import { Text, View } from 'react-native';
-import { WalkthroughSwiper } from 'src/WalkthroughSwiper';
+import {StyleSheet, Text, View} from 'react-native';
+import {WalkthroughSwiper} from 'react-native-walkthrough-swiper';
 
-const sampleData = [
+const imgs = [
+  'https://www.setaswall.com/wp-content/uploads/2017/10/Abstract-3-Wallpaper-1080x1920-768x1365.jpg',
+  'https://www.setaswall.com/wp-content/uploads/2017/10/Abstract-8-Wallpaper-1080x1920.jpg',
+  'https://www.setaswall.com/wp-content/uploads/2017/10/Artistic-Mountains-Qu-Wallpaper-1080x1920-768x1365.jpg',
+];
+
+const sliderData = [
   {
-    title: 'test 1',
-    subTitle: 'Subtitle 1',
+    title: 'Slide 1',
+    subTitle: 'Uses Reanimated API',
     age: 29,
-
-    bgImage:
-      'https://www.setaswall.com/wp-content/uploads/2017/10/Abstract-3-Wallpaper-1080x1920-768x1365.jpg',
-    // 'https://images.pexels.com/photos/12242508/pexels-photo-12242508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    bgImage: imgs[0],
   },
   {
-    title: 'test 2',
+    title: 'Slide 2',
+    subTitle: 'Smooth animations',
     age: 22,
-    bgImage:
-      'https://www.setaswall.com/wp-content/uploads/2017/10/Abstract-8-Wallpaper-1080x1920.jpg',
-    // 'https  ://images.pexels.com/photos/3062948/pexels-photo-3062948.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    bgImage: imgs[1],
   },
   {
-    title: 'test 3',
+    title: 'Slide 3',
+    subTitle: 'Fully customizable',
     age: 12,
-    bgImage:
-      'https://www.setaswall.com/wp-content/uploads/2017/10/Artistic-Mountains-Qu-Wallpaper-1080x1920-768x1365.jpg',
-    // 'https://images.pexels.com/photos/7663192/pexels-photo-7663192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-  },
-  {
-    title: 'test 4',
-    age: 12,
-    bgImage:
-      'https://github.com/naheed-shamim/react-native-walkthrough-swiper/blob/development/example/assets/duo-1.jpg?raw=true',
-    // 'https://images.pexels.com/photos/3894112/pexels-photo-3894112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    bgImage: imgs[2],
   },
 ];
 
 function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#fff' }}>
+    <View style={{flex: 1, backgroundColor: '#000'}}>
       <WalkthroughSwiper
-        data={sampleData}
+        data={sliderData}
         skipText={'Skip'}
-        nextButton={<Text style={{ color: '#fff' }}>{'test'}</Text>}
+        skipTextStyle={{color: '#ccc'}}
+        nextButton={<Text style={{color: '#ccc'}}>{'Next'}</Text>}
         centerComponent={
-          <Text style={{ fontSize: 40, color: 'white' }}>{'My App'}</Text>
+          <Text style={{fontSize: 40, color: 'white'}}>{'Your App'}</Text>
         }
-        skipTextStyle={{ color: 'white' }}
-        titleStyle={{ color: 'white' }}
-        subTitleStyle={{ color: 'white' }}
-        activeSlideColor={'teal'}
-        onSkipBtnPress={() => {}}
+        titleStyle={{color: 'white'}}
+        subTitleStyle={{color: 'white'}}
+        activeSlideColor={'#fff'}
+        inActiveSlideColor={'#b2b2b2'}
+        onSkipBtnPress={() => {
+          // Navigate to the next screen
+        }}
       />
     </View>
   );
 }
 
+const styles = StyleSheet.create({
+  flatlistRowItem: {
+    margin: 12,
+    borderWidth: 1,
+    padding: 12,
+    borderRadius: 8,
+  },
+});
+
 export default App;
+
+//  yarn add ./react-native-walkthrough-swiper-0.1.16.tgz
